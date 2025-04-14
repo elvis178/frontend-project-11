15df2e0a66a23e2f727c894f6c818954e117e776
elvis178 <elvisss178@gmail.com>
2025-04-15 00:07:39 +0300

    add locales

---
+++ src/locales/ru.js
@@ -0,0 +1,27 @@
+export default {
+  translation: {
+    validationErrors:  {
+      invalidUrl: 'Ссылка должна быть валидным URL',
+      invalidRss: 'Ресурс не содержит валидный RSS',
+      duplicateFeed: 'RSS уже существует',
+      requiredField: 'Поле не должно быть пустым',
+    },
+    networkErrors:  {
+      connectionFailed: 'Ошибка сети',
+      unknownError: 'Что-то пошло не так',
+    },
+    feedStates: {
+      loading: 'RSS загружается',
+      loaded: 'RSS успешно загружен',
+    },
+    ui: {
+      sections: {
+        feeds: 'Фиды',
+        posts: 'Посты',
+      },
+      buttons: {
+        view: 'Просмотр',
+      },
+    },
+  },
+};
