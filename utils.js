eaa2c5c2512f3c185d76042c2da688983cf9d136
elvis178 <elvisss178@gmail.com>
2025-04-24 18:09:33 +0300

    update utils.js

--- utils.js
+++ utils.js
@@ -1,25 +1,26 @@
 import * as yup from 'yup';
-import resources from './src/locales/locales.js';
+import local from './src/locales/locales.js';
 
 export const getProxyUrl = (url) => {
-  const proxyUrl = new URL('/get', 'https://allorigins.hexlet.app/get?');
+  const proxyUrl = new URL('https://allorigins.hexlet.app/get');
   proxyUrl.searchParams.set('disableCache', 'true');
   proxyUrl.searchParams.set('url', url);
   return proxyUrl.toString();
 };
 
-yup.setLocale(resources);
+yup.setLocale(local);
 
-const validate = (url, urlFeeds) => {
+const createUrlValidator = (url, urlFeeds) => {
   const schema = yup.object().shape({
     url: yup
       .string()
+      .transform(value => value?.trim())
       .url('errors.invalidUrl')
-      .trim()
       .required()
       .notOneOf(urlFeeds, 'errors.existsRss'),
   });
   return schema.validate({ url });
 };
 
-export default validate;
+
+export default createUrlValidator;
