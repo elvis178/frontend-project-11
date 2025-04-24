22354b6200483a52acebfcbc579489c0099fcd1a
elvis178 <elvisss178@gmail.com>
2025-04-24 23:26:51 +0300

    fix eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,25 +4,23 @@ import pluginJs from "@eslint/js";
 /** @type {import('eslint').Linter.Config} */
 export default [
   {
+    ignores: ["dist/**", "node_modules/**"]
+  },
+  {
     files: ["**/*.js"],
     languageOptions: {
       globals: {
-        ...globals.browser,
         ...globals.node,
+        ...globals.browser,
       },
       parserOptions: {
         ecmaVersion: "latest",
         sourceType: "module",
       },
     },
-  },
-  {
-    files: ["webpack.config.js"],
-    languageOptions: {
-      globals: {
-        ...globals.node,
-      },
-    },
+    rules: {
+      "no-undef": "off"
+    }
   },
   pluginJs.configs.recommended,
 ];
