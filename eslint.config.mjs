6367029fce6673458375d380d1ea68bca05d3f52
elvis178 <elvisss178@gmail.com>
2025-04-24 23:06:36 +0300

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,26 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.Config} */
 export default [
   {
+    files: ["**/*.js"],
     languageOptions: {
       globals: {
         ...globals.browser,
-        ...globals.node, 
+        ...globals.node,
+      },
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+  },
+  {
+    files: ["webpack.config.js"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
       },
     },
   },
